6f5b8c2acb3e7dd40bf9483d17eed392bf29682e
Tsenguun <kim.tsenguun@yahoo.com>
2017-05-24 10:24:00 +0900

    enable socket program

---
+++ server.py
@@ -0,0 +1,25 @@
+import sys, socket
+
+IP = "127.0.0.1"
+PORT = 8888
+
+print("Start programm")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+address = (IP, PORT)
+
+s.bind(address)
+s.listen(1)
+
+print("Start accept")
+cs, caddress = s.accept()
+print(caddress)
+
+while True:
+    data = cs.recv(1024)
+    if not data:
+        break
+    cs.sendall(data)
+    print("Client : %s" % data.decode())
+
+s.close()
